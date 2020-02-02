7827c349a42acd7e4e4f7ba95128070d36788d47
Cole O'Brien <>
2020-02-01 17:26:26 -0800

    Reorganized scripts and added bullet firing

---
+++ Assets/Other Cole Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Vector3 offset;
+    public float speed = 0.1f;
+    public Transform player;
+
+
+    void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Lerp(transform.position.x, player.position.x + offset.x, speed),
+            Mathf.Lerp(transform.position.y, player.position.y + offset.y, speed),
+            transform.position.z
+        );
+    }
+}
