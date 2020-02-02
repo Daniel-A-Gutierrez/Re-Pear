2b0d85422905cb205c1bba712ce39279241d58b9
Cole O'Brien <>
2020-02-02 14:12:40 -0800

    Added UI Canvas

---
+++ Assets/LevelNumber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelNumber : MonoBehaviour
+{
+
+    Text text;
+    LevelManager levelManager;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        levelManager = GameManager.instance.managers.GetComponent<LevelManager>();
+    }
+
+    void Update()
+    {
+        int level = levelManager.getLevel(); 
+        text.text = level.ToString();
+    }
+}
---
+++ Assets/XpBlend.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XpBlend : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    LevelManager levelManager;
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        levelManager = GameManager.instance.managers.GetComponent<LevelManager>();
+    }
+
+    void Update()
+    {
+        spriteRenderer.material.SetFloat("_Blend", 1 - (levelManager.getExp() / 4.0f));   
+    }
+}
