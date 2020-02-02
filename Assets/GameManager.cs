d85ac8217e73768ad1528f2f21ee8b85472347e8
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2020-02-01 17:24:02 -0800

    gameManager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,12 +6,21 @@ public class GameManager : MonoBehaviour
 {
     public GameObject stationBullet;
     Queue<GameObject> stationBullets;
+    public GameObject playerBullet;
+    Queue<GameObject> playerBullets;
+    public GameObject enemyBullet;
+    Queue<GameObject> enemyBullets;
+    public GameObject ant;
+    Queue<GameObject> ants;
+    public GameObject wasp;
+    Queue<GameObject> wasps;
     GameManager instance ;
 
     void Awake()
     {
         instance = this;
         stationBullets = new Queue<GameObject>();
+        transform.position = Vector3.zero;
     }
     // Start is called before the first frame update
     void Start()
@@ -29,14 +38,26 @@ public class GameManager : MonoBehaviour
     {
         if(stationBullets.Count == 0)
         {
-            GameObject bullet = Instantiate(stationBullet,position,Quaternion.identity );
+            GameObject bullet = Instantiate(stationBullet,position,Quaternion.identity ,transform);
             bullet.transform.up = direction.normalized;
-
+            bullet.GetComponent<Bullet>().direction = direction;
+            bullet.GetComponent<Bullet>().range = range;
+            bullet.GetComponent<Bullet>().startingPosition = position;
+        }
+        else
+        {
+            GameObject bullet = enemyBullets.Dequeue();
+            bullet.SetActive( true );
+            bullet.transform.up = direction.normalized;
+            bullet.GetComponent<Bullet>().direction = direction;
+            bullet.GetComponent<Bullet>().range = range;
+            bullet.GetComponent<Bullet>().startingPosition = position;
         }
     }
 
     void TerminateStationBullet(GameObject bullet)
     {
-        
+        bullet.SetActive(false);
+        stationBullets.Enqueue(bullet);
     }
 }
