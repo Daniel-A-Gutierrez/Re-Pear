d87521303a45cdbff1caa69a1c914943de04a485
Daniel Allen Gutierrez <icanmakespaghetti@gmail.com>
2020-02-01 18:45:19 -0800

    Bullets are in

---
+++ Assets/PlayerBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : Bullet
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = direction*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time-birthTime > lifetime)
+            Terminate();
+    }
+
+    public override void Terminate()
+    {
+        GameManager.instance.TerminatePlayerBullet(gameObject);
+    }
+
+}
